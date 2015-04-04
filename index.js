33279940aaa0f5dc0385ed9c7965b32a827b5787
Alvin Liang <liangalvin@yahoo.com>
2015-04-04 00:55:46 -0400

    added a counter to test the setinterval, partially working?

--- index.js
+++ index.js
@@ -15,26 +15,26 @@ function buildGrid() {
 //build checkboxes
 //append checkboxes
 
- var row;
- var col;
- var cell;
+  var row;
+  var col;
+  var cell;
 
- for( row = 0; row < rows; row++ ) {
-   cells[row] = [];
-   states[row] = [];
+  for( row = 0; row < rows; row++ ) {
+    cells[row] = [];
+    states[row] = [];
 
-   for( col = 0; col < cols; col++ ) {
-     cell = document.createElement("input");
-     cell.setAttribute("type", "checkbox");
+    for( col = 0; col < cols; col++ ) {
+      cell = document.createElement("input");
+      cell.setAttribute("type", "checkbox");
 
-     cells[row][col] = cell;
-     states[row][col] = 0;
+      cells[row][col] = cell;
+      states[row][col] = 0;
 
-     grid.appendChild(cell)
-     // cell.checked=true;
+      grid.appendChild(cell)
+      // cell.checked=true;
 
-   }
- }
+    }
+  }
 }
 
 function randomize() {
@@ -43,80 +43,92 @@ function randomize() {
 //random state either 1(checked) or 0(not)
 //input box is either checked or not
 
- var row;
- var col;
+  var row;
+  var col;
 
- for( row = 0; row < rows; row++ ) {
-   for( col = 0; col < cols; col++ ) {
-     states[row][col] = Math.round(Math.random());
+  for( row = 0; row < rows; row++ ) {
+    for( col = 0; col < cols; col++ ) {
+      states[row][col] = Math.round(Math.random());
      //console.log(Math.random());
-     if(states[row][col] === 1 && Math.random() > .7){
-       cells[row][col].checked = true;
-     }
-   }
- }
+      if(states[row][col] === 1 && Math.random() > .7){
+        cells[row][col].checked = true;
+      }
+    }
+  }
 }
 
 function updateGrid(){
 
- var row;
- var col;
- var num;
+  var row;
+  var col;
+  var num;
 
  // update states from last update
 
- for( row = 0; row < rows; ++row ) {
-   for( col = 0; col < cols; ++col ) {
-     if( states[row][col].checked === 1 ) {
-       cells[row][col].checked = true;
-     }
-   }
- }
+  for( row = 0; row < rows; ++row ) {
+    for( col = 0; col < cols; ++col ) {
+      if( states[row][col].checked === 1 ) {
+        cells[row][col].checked = true;
+      }
+    }
+  }
 
- for( row = 0; row < rows; ++row ) {
-   for( col = 0; col < cols; ++col ) {
-     numCell = 0;
+  for( row = 0; row < rows; ++row ) {
+    for( col = 0; col < cols; ++col ) {
+      numCell = 0;
 
-     // North
-     if( row > 0 )
-       numCell += states[row - 1][col];
-     // NorthEast
-     if( row > 0 && col < cols - 1 ) numCell += states[row - 1][col + 1];
-     // East
-     if( col < cols - 1 )
-       numCell += states[row][col + 1];
-     // SouthEast
-     if( row < rows - 1 && col < cols - 1 )
-       numCell += states[row + 1][col + 1];
-     // South
-     if( row < rows - 1 )
-       numCell += states[row + 1][col];
-     // SouthWest airlines
-     if( row < rows - 1 && col > 0 )
-       numCell += states[row + 1][col - 1];
-     // West
-     if( col > 0 )
-       numCell += states[row][col - 1];
-     // NorthWest
-     if( col > 0 && row > 0 )
-       numCell += states[row - 1][col - 1];
+      // North
+      if( row > 0 ){
+        numCell += states[row - 1][col];
+      }
+      // NorthEast
+      else if( row > 0 && col < cols - 1 ){
+        numCell += states[row - 1][col + 1];
+      }
+      // East
+      if( col < cols - 1 ){
+        numCell += states[row][col + 1];
+      }
+      // SouthEast
+      if( row < rows - 1 && col < cols - 1 ){
+        numCell += states[row + 1][col + 1];
+      }
+      // South
+      if( row < rows - 1 ){
+        numCell += states[row + 1][col];
+      }
+      // SouthWest airlines
+      if( row < rows - 1 && col > 0 ){
+        numCell += states[row + 1][col - 1];
+      }
+      // West
+      if( col > 0 ){
+        numCell += states[row][col - 1];
+      }
+      // NorthWest
+      if( col > 0 && row > 0 ){
+        numCell += states[row - 1][col - 1];
+      }
 
-     //cell row col checked and its 2 in a row or 3 in a row
-     //fix this
-     if( states[row][col].checked === true && numCell < 2 ) {
-        cells[row][col].checked = 0;
-     }
-    //  else {
-    //    cells[row][col].checked = 0;
-    //  }
-   }
- }
- gen++;
+      //cell row col checked and its 2 in a row or 3 in a row
+      //fix this
+      if( cells[row][col].checked === 1 && (numCell === 2 || numCell === 3) ) {
+         states[row][col].checked = true;
+      }
+      else {
+        states[row][col].checked = false;
+      }
+    }
+  }
+  gen++;
 }
 
 function generateCells() {
+  var counter = 0;
   setInterval( function() {
     updateGrid();
+    counter ++;
+    console.log(counter)
   }, 1000 );
 }
 
@@ -127,5 +139,6 @@ button.addEventListener("click", function(evt) {
    buildGrid();
    gen = 0;
    randomize();
-   generateCells();
+   generateCells()
+   //setInterval( function() { updateGrid(); }, 1000 );
 });
