363c43650d9985ce719358c6f1c70f6725472f05
Alvin Liang <liangalvin@yahoo.com>
2015-04-05 20:52:43 -0400

    added file organism.js almost done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var gen = 0;
+var generations = 0;
 var rows = 20;
 var cols = 20;
 var total = rows*cols;
@@ -35,12 +35,11 @@ function buildGrid() {
 
     }
   }
+  //console.log(cells);
 }
 
 function randomize() {
-//var rand = Math.round(Math.random());
-
-//random state either 1(checked) or 0(not)
+//random state either 1(checked for alive) or 0(not)
 //input box is either checked or not
 
   var row;
@@ -49,8 +48,7 @@ function randomize() {
   for( row = 0; row < rows; row++ ) {
     for( col = 0; col < cols; col++ ) {
       states[row][col] = Math.round(Math.random());
-     //console.log(Math.random());
-      if(states[row][col] === 1 && Math.random() > .7){
+      if( states[row][col] === 1 && Math.random() > .7 ){
         cells[row][col].checked = true;
       }
     }
@@ -58,10 +56,9 @@ function randomize() {
 }
 
 function updateGrid(){
-
   var row;
   var col;
-  var num;
+  var numCell;
 
  // update states from last update
 
@@ -70,6 +67,9 @@ function updateGrid(){
       if( states[row][col].checked === 1 ) {
         cells[row][col].checked = true;
       }
+      // else {
+      //   cells[row][col].checked = false;
+      // }
     }
   }
 
@@ -78,62 +78,63 @@ function updateGrid(){
       numCell = 0;
 
       // North
-      if( row > 0 ){
+      if( row > 0 ) {
         numCell += states[row - 1][col];
       }
       // NorthEast
-      else if( row > 0 && col < cols - 1 ){
+      if( row > 0 && col < cols - 1 ) {
         numCell += states[row - 1][col + 1];
       }
       // East
-      if( col < cols - 1 ){
+      if( col < cols - 1 ) {
         numCell += states[row][col + 1];
       }
       // SouthEast
-      if( row < rows - 1 && col < cols - 1 ){
+      if( row < rows - 1 && col < cols - 1 ) {
         numCell += states[row + 1][col + 1];
       }
       // South
-      if( row < rows - 1 ){
+      if( row < rows - 1 ) {
         numCell += states[row + 1][col];
       }
       // SouthWest airlines
-      if( row < rows - 1 && col > 0 ){
+      if( row < rows - 1 && col > 0 ) {
         numCell += states[row + 1][col - 1];
       }
       // West
-      if( col > 0 ){
+      if( col > 0 ) {
         numCell += states[row][col - 1];
       }
       // NorthWest
-      if( col > 0 && row > 0 ){
+      if( col > 0 && row > 0 ) {
         numCell += states[row - 1][col - 1];
       }
 
+      //console.log(numCell);
       //cell row col checked and its 2 in a row or 3 in a row
-      //fix this
+      //        ******* fix this ********
       if( cells[row][col].checked === 1 && (numCell === 2 || numCell === 3) ) {
          states[row][col].checked = true;
       }
+      //cell is by itself
       else {
         states[row][col].checked = false;
       }
     }
   }
-  gen++;
+  generations++;
 }
 
 function generateCells() {
-  var counter = 0;
+  //var counter = 0;
   setInterval( function() {
     updateGrid();
-    counter ++;
-    console.log(counter)
+    //counter ++;
+    //console.log(counter)
   }, 1000 );
 }
 
-//buildGrid();
-
+//buildGrid > random cells generated > update every second
 button.addEventListener("click", function(evt) {
    console.log("clicked");
    buildGrid();
---
+++ organism.js
@@ -0,0 +1,148 @@
+var rows = 20;
+var cols = 20;
+var gridScheme = [];
+var state = [];
+var grid = document.querySelector("#grid");
+var button = document.querySelector("#generate");
+var body = document.querySelector("body");
+
+//1. build the grid
+function buildGrid() {
+  var x; //x axis
+  var y; //y axis
+  var position; //the position on the grid
+  var num = 0;
+
+  for(x = 0; x < rows; x++) {
+    gridScheme[x] = [];
+    state[x] = [];
+
+    for(var y = 0; y < cols; y++) {
+      position = document.createElement("input");
+      position.setAttribute("type", "checkbox");
+      position.id = num;
+      num++;
+
+      gridScheme[x][y] = position;
+      state[x][y] = 0;
+
+      grid.appendChild(position);
+    }
+  }
+}
+
+//2. randomly put organism onto grid (state 1 = checked or alive / 0 = unchecked or dead)
+function randomize() {
+  var x;
+  var y;
+  var randNum; //only allow 30% of the grid to be generated with alive organisms
+
+  for(x = 0; x < rows; x++) {
+    for(y = 0; y < rows; y++) {
+
+      randNum = Math.random();
+      state[x][y] = Math.round(Math.random());
+
+      if(state[x][y] === 1 && randNum > .7) {
+        gridScheme[x][y].checked = true;
+      }
+    }
+  }
+}
+
+//3.check grid for checkedboxes
+//3.1. check surrounding boxes ->
+//single array(the grid) with 20 arrays, each with 20 check boxes
+//3.2. get rid of single boxes
+function checkGrid(grid) {
+  grid.forEach(function(ary) {
+    ary.forEach(function(box) {
+
+      //var banned = document.getElementById(box.id-1); //do not allow
+      var north = document.getElementById(box.id-20);
+      var northEast = document.getElementById(box.id-19);
+      var northWest = document.getElementById(box.id-21);
+      var east = document.getElementById(parseInt(box.id)+1);
+      var west = document.getElementById(box.id-1);
+      var south = document.getElementById(parseInt(box.id)+20);
+      var southEast = document.getElementById(parseInt(box.id)+21);
+      var southWest = document.getElementById(parseInt(box.id)+19);
+      var boxConnect = 0;
+
+      // if((box.id % 20 === 0 && box.checked === true) && banned.checked === true) {
+      //   console.log(box.id + " " + banned.id);
+      // }
+      if((box.checked === true && box.id > 19) && north.checked === true) {
+        //console.log("North: " + box.id + " " + north.id);
+        boxConnect++;
+      }
+      if((box.checked === true && box.id > 19) && northEast.checked === true) {
+        //console.log("Northeast: " + box.id + " " + northEast.id);
+        boxConnect+=2;
+      }
+      if((box.checked === true && box.id % 20 !== 0) && (box.id > 19 && northWest.checked === true)) {
+        //console.log("Northwest: " + box.id + " " + northWest.id);
+        boxConnect+=2;
+      }
+      if((box.checked === true && (parseInt(box.id)+1)%20 !== 0) && east.checked === true) {
+        //console.log("East: " + box.id + " " + east.id);
+        boxConnect++;
+      }
+      if(box.checked === true && west.checked === true && (box.id-1)%20 !== 19) {
+        //console.log("West: " + box.id + " " + west.id);
+        boxConnect++;
+      }
+      if((box.checked === true && box.id < 380) && south.checked === true) {
+        //console.log("South: " + box.id + " " + south.id);
+        boxConnect++;
+      }
+      if(box.checked === true && (box.id < 380) && southEast.checked === true) {
+        //console.log("Southeast: " + box.id + " " + southEast.id);
+        boxConnect+=2;
+      }
+      if((box.checked === true && box.id < 380) && southWest.checked === true) {
+        //console.log("Southwest: " + box.id + " " + southWest.id);
+        boxConnect+=2;
+      }
+
+      function ridSingles(){
+        if(boxConnect >= 2) {
+          box.checked = true;
+        }
+        else {
+          box.checked = false;
+        }
+      }
+
+      function moveBoxes(){
+        var spaces = [1, -1, 19, 20, 21, -19, -20, -21];
+        var randSpace = Math.floor(Math.random()*spaces.length);
+        //console.log(randSpace);
+        if(box.checked === true && box.id < 400 || box.id > 0){
+          box.id = parseInt(box.id)+ parseInt(spaces[randSpace]);
+          console.log(parseInt(box.id) + parseInt(spaces[randSpace]));
+          box.id = parseInt(box.id);
+        }
+        else if(box.checked === true && box.id > 400 || box.id < 0){
+          box.checked = false;
+        }
+      }
+
+      setInterval(function(){
+        ridSingles();
+        moveBoxes();
+      }, 1000);
+
+    });
+
+    //console.log(ary);
+  });
+}
+
+button.addEventListener("click", function() {
+  console.log("clicked");
+  checkGrid(gridScheme);
+});
+
+buildGrid();
+randomize();
